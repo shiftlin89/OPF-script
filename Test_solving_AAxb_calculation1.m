a3364869e47b4b0e58c6c6e9a394fe1aed7ad2aa
shiftlin <31167007+shiftlin89@users.noreply.github.com>
2024-08-09 12:36:54 -0400

    Update Test_solving_AAxb_calculation1.m

--- Test_solving_AAxb_calculation1.m
+++ Test_solving_AAxb_calculation1.m
@@ -1,3 +1,26 @@
+Comparison of Results:
+Direct Computation time: 0.370163 seconds.
+LSQR time: 0.407603 seconds.
+SVD time: 22.166413 seconds.
+QR time: 3.701567 seconds.
+
+Differences between solutions:
+Norm between Direct and LSQR: 8.128928e-01
+Norm between Direct and SVD: 2.984406e-13
+Norm between Direct and QR: 1.574757e-13
+Norm between LSQR and SVD: 8.128928e-01
+Norm between LSQR and QR: 8.128928e-01
+Norm between SVD and QR: 2.672493e-13
+
+Verification of Residuals:
+Norm of residual for Direct Computation: 1.255158e-12
+Norm of residual for LSQR: 4.180804e-01
+Norm of residual for SVD: 3.685822e-12
+Norm of residual for QR: 1.973699e-12
+
+
+
+
 % Generate a non-square sparse matrix A (800x1000)
 n = 4800;  % Number of rows
 m = 4000;  % Number of columns
